7c0e8bc8c2f410e5cd4fcf68d02c70881d88a617
JasonCheung90 <574457105@qq.com>
2020-06-24 10:32:15 +0800

    edit goods

--- src/components/Goods.vue
+++ src/components/Goods.vue
@@ -77,6 +77,31 @@
         :total="total">
       </el-pagination>
     </el-card>
+    <!-- 修改商品对话框 -->
+    <el-dialog
+      title="编辑修改商品"
+      :visible.sync="goodEditDialog"
+      width="50%"
+     >
+      <el-form :model="goodEditForm" :rules="goodRules" ref="goodEditFormRef" label-width="100px">
+        <el-form-item label="商品名称" prop="goods_name">
+              <el-input v-model="goodEditForm.goods_name"></el-input>
+            </el-form-item>
+            <el-form-item label="商品价格" prop="goods_price">
+              <el-input v-model="goodEditForm.goods_price" type="number"></el-input>
+            </el-form-item>
+            <el-form-item label="商品重量" prop="goods_weight">
+              <el-input v-model="goodEditForm.goods_weight" type="number"></el-input>
+            </el-form-item>
+            <el-form-item label="商品数量" prop="goods_number">
+              <el-input v-model="goodEditForm.goods_number" type="number"></el-input>
+            </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="goodEditDialog = !goodEditDialog">取 消</el-button>
+        <el-button type="primary" @click="goodEditCommit">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -98,7 +123,34 @@ export default {
       // 显示修改商品信息对话框开关
       goodsEditDialog: false,
       // 商品数据列表
-      goodList: []
+      goodList: [],
+      goodEditDialog: false,
+      // 商品表单
+      goodEditForm: {
+        goods_name: '',
+        goods_price: 0,
+        goods_number: 0,
+        goods_weight: 0,
+        goods_cat: [],
+        pics: [],
+        goods_introduce: '',
+        attrs: []
+      },
+      goodRules: {
+        goods_name: [
+          { required: true, message: '请输入商品名', trigger: 'blur' },
+          { min: 3, max: 100, message: '长度在 3 到 100个字符', trigger: 'blur' }
+        ],
+        goods_price: [
+          { required: true, message: '请输入价格', trigger: 'blur' }
+        ],
+        goods_number: [
+          { required: true, message: '请输入数量', trigger: 'blur' }
+        ],
+        goods_weight: [
+          { required: true, message: '请输入重量', trigger: 'blur' }
+        ]
+      }
     }
   },
   methods: {
@@ -123,7 +175,30 @@ export default {
     resetGoodForm () {
       this.$refs.addGoodFormRef.resetFields()
     },
-    showEditGood () {},
+    // 编辑商品
+    async showEditGood (id) {
+      this.goodEditDialog = !this.goodEditDialog
+      const { data: result } = await this.axios.get(`goods/${id}`)
+      if (result.meta.status !== 200) {
+        return this.$message.error('查询商品数据失败')
+      }
+      this.goodEditForm = result.data
+    },
+    // 编辑商品提交
+    goodEditCommit () {
+      this.$refs.goodEditFormRef.validate(async check => {
+        if (!check) return false
+        const { data: result } = await this.axios.put(`goods/${this.goodEditForm.goods_id}`, this.goodEditForm)
+        console.log(result)
+
+        if (result.meta.status !== 200) {
+          return this.$message.error('修改商品失败')
+        }
+        this.goodEditDialog = !this.goodEditDialog
+        this.getGoodList()
+        this.$message.success(result.meta.msg)
+      })
+    },
     // 根据商品ID删除对应的商品
     async deleteGoods (goodsId) {
       const confirmRes = await this.$confirm('此操作将永久删除该商品, 是否继续?', '提示', {

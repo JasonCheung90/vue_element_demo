eeb6dd3de6299defe54ad4378d63590ae0845541
JasonCheung90 <574457105@qq.com>
2020-07-01 17:24:28 +0800

    finish reports

--- src/components/Reports.vue
+++ src/components/Reports.vue
@@ -1,12 +1,71 @@
 <template>
   <div>
-    <h3>reports</h3>
+    <!-- 面包屑导航 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ name: 'home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>数据统计</el-breadcrumb-item>
+      <el-breadcrumb-item>数据报表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card>
+       <div id="main" style="width: 800px;height:400px;"></div>
+    </el-card>
   </div>
 </template>
 
 <script>
+import echarts from 'echarts'
+import _ from 'lodash'
 export default {
-  name: 'reports'
+  name: 'reports',
+  data () {
+    return {
+      reportObj: {
+        title: {
+          text: '用户来源'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            label: {
+              backgroundColor: '#E9EEF3'
+            }
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            boundaryGap: false
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ]
+      }
+    }
+  },
+  methods: {
+    async getReportList () {
+      const myChart = echarts.init(document.getElementById('main'))
+      const { data: result } = await this.axios.get('reports/type/1')
+      if (result.meta.status !== 200) return this.$message.error('获取数据报表失败')
+      const options = _.merge(result.data, this.reportObj)
+      myChart.setOption(options)
+    }
+  },
+  created () {
+
+  },
+  mounted () {
+    this.getReportList()
+  }
 }
 </script>
 <style lang="less" scoped>
